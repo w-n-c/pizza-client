bb158708cc620d558537d1bb1d20f3711c345915
Dylan graham <43048723+DylanGraham140@users.noreply.github.com>
2020-05-04 19:33:27 -0400

    The button in the picture are linked to other pages
    The button hidden the pictures have been centered

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -9,6 +9,18 @@ export class LandingComponent implements OnInit {
 
   constructor() { }
 
+
+
+  async signup(){
+    let link = location.protocol + '//' + location.host + location.pathname +'/signup';
+    window.location.replace(link);
+  }
+
+  async orderNow(){
+    let link = location.protocol + '//' + location.host + location.pathname +'/order';
+    window.location.replace(link);
+  }
+
   ngOnInit(): void {
   }
 
