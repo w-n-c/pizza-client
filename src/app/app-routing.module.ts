670be59b9de181033715c1670bc3d6fc8e599c58
Will Newell <newellwm@gmail.com>
2020-05-04 22:08:34 -0400

    Code cleanup, route refactor, Model refactor
    - Code cleanup consisted of simpler object creation techniques, better
      line spacing, removing excess console.logs etc.
    - Rewrote routes in app to use angular's routing module to eliminate
      hard page refreshes and speed up load times.
    - Refactored several components to make better use of the new models,
      and removed model constructors to simplify Model instantiaion with
      partial variables.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
-import {PizzaOrderComponent} from './components/pizza-order/pizza-order.component';
+import { LoginComponent } from './components/login/login.component';
+import { PizzaOrderComponent } from './components/pizza-order/pizza-order.component';
 import { EmployeeViewComponent } from './components/employee-view/employee-view.component';
-import {RegisteredCustomerComponent} from './components/registered-customer/registered-customer.component';
-import {RegisterComponent} from './components/register/register.component'
-import {LandingComponent} from './components/landing/landing.component'
+import { RegisteredCustomerComponent } from './components/registered-customer/registered-customer.component';
+import { RegisterComponent } from './components/register/register.component'
+import { LandingComponent } from './components/landing/landing.component'
 
 const routes: Routes = [
-  {path:"",component:LoginComponent},
-  {path:"order",component:PizzaOrderComponent},
+  {path: "", component:LoginComponent},
+  {path: "order",component:PizzaOrderComponent},
   {path: "registeredcustomer", component:RegisteredCustomerComponent},
   {path: "signup", component:RegisterComponent},
   {path: "landing", component:LandingComponent},
-  {path: "employee/order", component:EmployeeViewComponent}
+  {path: "employee", component:EmployeeViewComponent}
 ];
 
 @NgModule({
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,15 +1,10 @@
-import {Ticket} from 'src/app/models/ticket'
+import { Ticket } from 'src/app/models/ticket'
+import { Role } from './role';
 
 export class User{
     id:number;
     username:string;
     password:string;
     tickets:Array<Ticket>;
-
-    constructor(id,username,password,tickets){
-        this.id=id;
-        this.username=username;
-        this.password=password;
-        this.tickets=tickets;
-    }
+    role:Role;
 }
